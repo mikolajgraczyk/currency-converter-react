2d200c1611cdab2571a9bbfd6b665acd7f0f6224
Mikołaj Graczyk <mikolajgraczyk123@gmail.com>
2023-01-11 22:20:09 +0100

    removed unnecessery spaces in useCurrentDate.js

--- src/Section/DateAndTime/useCurrentDate.js
+++ src/Section/DateAndTime/useCurrentDate.js
@@ -1,15 +1,15 @@
 import { useState, useEffect } from "react";
 
 export const useCurrentDate = () => {
-    const [myDate, setMyDate] = useState(new Date());
+    const [currentDate, setCurrentDate] = useState(new Date());
 
     useEffect(() => {
-        const intervalId = setInterval(() => setMyDate(new Date()), 1000);
+        const intervalId = setInterval(() => setCurrentDate(new Date()), 1000);
 
         return () => {
             clearInterval(intervalId);
         };
     }, []);
 
-    return myDate;
+    return currentDate;
 };
