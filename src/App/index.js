512ed2f951eb95ef8ba89830ae693d0d73855cae
Mikołaj Graczyk <mikolajgraczyk123@gmail.com>
2023-01-12 22:26:21 +0100

    formated App/index.js code

--- src/App/index.js
+++ src/App/index.js
@@ -8,56 +8,56 @@ import { currencies } from '../utils/currencies';
 import { Container, Converter, Aside, ResultSection } from './styled';
 
 function App() {
-  const [result, setResult] = useState();
-  const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
+    const [result, setResult] = useState();
+    const [theme, setTheme] = useState(localStorage.getItem("theme") || "dark");
 
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-  }, [theme]);
+    useEffect(() => {
+        localStorage.setItem("theme", theme);
+    }, [theme]);
 
-  const calculateResult = (firstSelectValue, secondSelectValue, firstInputValue) => {
-    const firstSelectedCurrency = currencies.find(({ short }) => short === firstSelectValue);
-    const secondSelectedCurrency = currencies.find(({ short }) => short === secondSelectValue);
-    const rate = secondSelectedCurrency.rate / firstSelectedCurrency.rate;
+    const calculateResult = (firstSelectValue, secondSelectValue, firstInputValue) => {
+        const firstSelectedCurrency = currencies.find(({ short }) => short === firstSelectValue);
+        const secondSelectedCurrency = currencies.find(({ short }) => short === secondSelectValue);
+        const rate = secondSelectedCurrency.rate / firstSelectedCurrency.rate;
 
-    setResult({
-      firstInputValue,
-      fromCurrency: firstSelectedCurrency.short,
-      calculated: (rate * firstInputValue).toFixed(2),
-      toCurrency: secondSelectedCurrency.short,
-    })
-  }
+        setResult({
+            firstInputValue,
+            fromCurrency: firstSelectedCurrency.short,
+            calculated: (rate * firstInputValue).toFixed(2),
+            toCurrency: secondSelectedCurrency.short,
+        })
+    }
 
 
-  const themeToggle = () => {
-    theme === "light" ? setTheme("dark") : setTheme("light");
-  };
+    const themeToggle = () => {
+        theme === "light" ? setTheme("dark") : setTheme("light");
+    };
 
-  const isLightTheme = theme === "light";
+    const isLightTheme = theme === "light";
 
-  return (
-    <Container>
-    <Converter light={isLightTheme}>
-        <ThemeSwitch
-            themeToggle={themeToggle}
-            theme={theme}
-        />
-        <Header title="Przelicznik walut" />
-        <Form
-            calculateResult={calculateResult}
-            theme={theme}
-        />
-    </Converter>
-    <Aside light={isLightTheme}>
-        <DateAndTime />
-    </Aside>
-    {result !== undefined && (
-        <ResultSection light={isLightTheme}>
-            <Result result={result} />
-        </ResultSection>
-    )}
-</Container>
-  );
+    return (
+        <Container>
+            <Converter light={isLightTheme}>
+                <ThemeSwitch
+                    themeToggle={themeToggle}
+                    theme={theme}
+                />
+                <Header title="Przelicznik walut" />
+                <Form
+                    calculateResult={calculateResult}
+                    theme={theme}
+                />
+            </Converter>
+            <Aside light={isLightTheme}>
+                <DateAndTime />
+            </Aside>
+            {result !== undefined && (
+                <ResultSection light={isLightTheme}>
+                    <Result result={result} />
+                </ResultSection>
+            )}
+        </Container>
+    );
 };
 
 export default App;
