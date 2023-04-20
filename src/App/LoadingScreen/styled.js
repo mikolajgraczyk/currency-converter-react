c48db649dd9d436edbee03bd59cc4e244c298c98
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-04-20 15:38:21 +0200

    loading screen doesnt change dimensions

--- src/App/LoadingScreen/styled.js
+++ src/App/LoadingScreen/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const LoadingTextSection = styled.div`
     font-size: 30px;
+    height: 292.5px;
     justify-self: center;
     font-weight: 900;
     display: flex;
@@ -11,6 +12,7 @@ export const LoadingTextSection = styled.div`
      @media(max-width: ${({theme}) => theme.breakpoint.mobileMax}px) {
         font-size: 25px;
         gap: 10px;
+        height: 383.5px;
     }
 `;
 
