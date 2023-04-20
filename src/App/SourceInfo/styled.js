0ed41eee5b0d2b421b7edbc105bcc6f35560f5ac
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-04-20 15:30:01 +0200

    centered the source info

--- src/App/SourceInfo/styled.js
+++ src/App/SourceInfo/styled.js
@@ -3,10 +3,10 @@ import styled, { css } from "styled-components";
 export const SourceInfoSection = styled.div`
     font-size: 12px;
     margin-top: 20px;
+    text-align: center;
 
     @media(max-width: ${({ theme }) => theme.breakpoint.mobileMax}px){
         font-size: 10px;
-        text-align: center;
     };
 
     ${({ error }) => error && css`
