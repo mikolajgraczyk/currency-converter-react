a2963da4bf8c9c1a3c6d3385cbba763602e73203
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-11-07 20:16:29 +0100

    changed API provider

--- src/App/useCurrenciesApiFetch.js
+++ src/App/useCurrenciesApiFetch.js
@@ -2,39 +2,86 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 export const useCurrenciesApiFetch = () => {
-    const [apiResponse, setApiResponse] = useState({
-        status: "loading",
-    });
+  const [apiResponse, setApiResponse] = useState({
+    status: "loading",
+  });
 
-    const randomNumber = Math.floor(Math.random() * 100000);
+  useEffect(() => {
+    const fetchRates = () => {
+      axios
+        .get(
+          `https://api.currencyapi.com/v3/latest?apikey=cur_live_s3HPUzSUq9TAbPcMUyaTV5TYjjvBelJj5QiJKEgw&currencies=PLN%2CEUR%2CUSD%2CGBP%2CCHF&base_currency=PLN`
+        )
+        .then((currenciesApi) => {
+          const response = currenciesApi.data;
 
-    useEffect(() => {
-        const fetchRates = () => {
-            axios.get(`https://api.exchangerate.host/latest?BASE=PLN&rand=${randomNumber}`)
-                .then(currenciesApi => {
-                    const currencies = Object.keys(currenciesApi.data.rates).map(key => {
-                        return {
-                            short: key,
-                            rate: currenciesApi.data.rates[key],
-                        }
-                    });
-                    const rateDate = currenciesApi.data.date;
+          const rateDate = response.meta.last_updated_at;
+          const currencies = Object.keys(response.data).map((key) => {
+            return {
+              short: key,
+              rate: response.data[key].value,
+            };
+          });
 
-                    setApiResponse({
-                        status: "success",
-                        currencies,
-                        rateDate,
-                    });
-                })
-                .catch((error) => {
-                    console.error(`Wystąpił błąd: ${error}`);
-                    setApiResponse({
-                        status: "error",
-                    });
-                });
-        }
-        setTimeout(fetchRates, 1000);
-    }, []);
-    
-    return { apiResponse };
-}
+          setApiResponse({
+            status: "success",
+            currencies,
+            rateDate,
+          });
+        })
+        .catch((error) => {
+          console.error(`Wystąpił błąd: ${error}`);
+          setApiResponse({
+            status: "error",
+          });
+        });
+    };
+    setTimeout(fetchRates, 1000);
+  }, []);
+
+  return { apiResponse };
+};
+
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// export const useCurrenciesApiFetch = () => {
+//   const [apiResponse, setApiResponse] = useState({
+//     status: "loading",
+//   });
+
+//   useEffect(() => {
+//     const fetchRates = () => {
+//       axios
+//         .get(
+//           `https://api.currencyapi.com/v3/latest?apikey=cur_live_s3HPUzSUq9TAbPcMUyaTV5TYjjvBelJj5QiJKEgw&currencies=EUR%2CUSD%2CGBP%2CCHF&base_currency=PLN`
+//         )
+//         .then((currenciesApi) => {
+//           const currencies = Object.keys(currenciesApi.data.rates).map(
+//             (key) => {
+//               return {
+//                 short: key,
+//                 rate: currenciesApi.data.rates[key],
+//               };
+//             }
+//           );
+//           const rateDate = currenciesApi.data.date;
+
+//           setApiResponse({
+//             status: "success",
+//             currencies,
+//             rateDate,
+//           });
+//         })
+//         .catch((error) => {
+//           console.error(`Wystąpił błąd: ${error}`);
+//           setApiResponse({
+//             status: "error",
+//           });
+//         });
+//     };
+//     setTimeout(fetchRates, 1000);
+//   }, []);
+
+//   return { apiResponse };
+// };
