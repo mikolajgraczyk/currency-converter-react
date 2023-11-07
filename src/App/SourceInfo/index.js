6ec06b1ba88d1560abcc8b05d66904d78ec1b08b
Mikołaj Graczyk <118211320+mikolajgraczyk@users.noreply.github.com>
2023-11-07 20:19:32 +0100

    fixed date info

--- src/App/SourceInfo/index.js
+++ src/App/SourceInfo/index.js
@@ -1,9 +1,10 @@
 import { SourceInfoSection } from "./styled";
 
 const SourceInfo = ({ apiResponse }) => (
-    <SourceInfoSection error={apiResponse.status === "error"}>
-        Kursy pochodzą z baz danych Komisji Europejskiej z dnia <strong>{apiResponse.rateDate}</strong>
-    </SourceInfoSection>
+  <SourceInfoSection error={apiResponse.status === "error"}>
+    Kursy pochodzą z baz danych Komisji Europejskiej z dnia{" "}
+    <strong>{apiResponse.rateDate.substring(0, 10)}</strong>
+  </SourceInfoSection>
 );
 
 export default SourceInfo;
